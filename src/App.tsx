edbc59186294398bfe7ef3b1bcb91e42cd4ebc53
JeonDoGyun <wjsehrbs2632@naver.com>
2023-09-24 17:41:15 +0900

    init: 시스템 초기 설정
    create-react-app 사용
    typescript 사용
    tailwind 사용
    react-query, recoil 사용
    eslint, prettier 사용

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RecoilRoot } from "recoil";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div className="App"></div>;
+    </RecoilRoot>
+  );
+}
+
+export default App;
