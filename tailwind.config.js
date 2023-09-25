11f647bd3799c2280bf4273b883f25a383d40084
JeonDoGyun <wjsehrbs2632@naver.com>
2023-09-25 13:10:04 +0900

    style: Tailwindcss config 파일 스타일 수정
    prettier 규칙에 따라 큰 따옴표를 작은 따옴표로 변경했음

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
