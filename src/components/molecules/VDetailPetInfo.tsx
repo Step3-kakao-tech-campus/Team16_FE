677e7a5038c71888905485ef8741e6f895cff9f9
ba0061@naver.com <ba0061@naver.com>
2023-09-27 00:42:48 +0900

    feat: api 명세대로 인터페이스 변경
    아직 확정 아니며, 보호소 정보 빠져있음
    -> 추후 추가적인 변경 필요

--- src/components/molecules/VDetailPetInfo.tsx
+++ src/components/molecules/VDetailPetInfo.tsx
@@ -1,38 +1,50 @@
-interface VDetailPetInfoProps {
-  name: string;
-  age: string;
-  sex: string;
-  weight: number;
-  description: string;
-}
+import { MockDetailPetInfoProps } from 'components/organisms/VDetailPetData';
 
-const VDetailPetInfo = ({
-  name,
-  age,
-  sex,
-  weight,
-  description,
-}: VDetailPetInfoProps) => {
+const VDetailPetInfo = (data: MockDetailPetInfoProps) => {
   return (
     <div className="flex flex-col gap-10 items-center">
-      <h2 className="text-3xl">{name}</h2>
+      <h2 className="text-3xl">{data.name}</h2>
+
       <div className="flex gap-10 justify-around">
-        <div className="w-20">
-          <span className="font-bold">나이</span> {age}{' '}
+        <div className="w-32">
+          <span className="font-bold">나이</span> {data.age}{' '}
         </div>
         <div className="w-48">
-          <span className="font-bold">성별</span> {sex}
+          <span className="font-bold">성별</span> {data.sex}
         </div>
       </div>
+
       <div className="flex gap-10 justify-around">
-        <div className="w-20">
-          <span className="font-bold">몸무게</span> {weight}kg{' '}
+        <div className="w-32">
+          <span className="font-bold">몸무게</span> {data.weight}kg{' '}
         </div>
         <div className="w-48">
           <span className="font-bold">분양지역</span> 광주북구동물보호소
         </div>
       </div>
-      <div>{description}</div>
+
+      <div className="flex gap-10 justify-around">
+        <div className="w-32">
+          <span className="font-bold">백신접종</span>
+          {` ${data.vaccinationStatus}`}{' '}
+        </div>
+        <div className="w-48">
+          <span className="font-bold">중성화</span>
+          {` ${data.neutralizationStatus}`}
+        </div>
+      </div>
+
+      <div className="flex gap-10 justify-around">
+        <div className="w-32">
+          <span className="font-bold">입양 여부</span>
+          {` ${data.adoptionStatus}`}{' '}
+        </div>
+        <div className="w-48">
+          <span className="font-bold">보호종료일</span>
+          {` ${data.protectionExpirationDate}`}
+        </div>
+      </div>
+      <div>{data.description}</div>
     </div>
   );
 };
