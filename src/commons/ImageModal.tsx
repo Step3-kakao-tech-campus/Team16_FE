de60497b696083f4968fba63e7a68c3d887b50be
ba0061@naver.com <ba0061@naver.com>
2023-09-28 02:04:27 +0900

    feat: 모달 닫는 버튼 크기 증가

--- src/commons/ImageModal.tsx
+++ src/commons/ImageModal.tsx
@@ -14,8 +14,10 @@ const ImageModal = ({
       onClick={handleModalOutsideClick}
       className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
     >
-      <div className="flex w-4/5 flex-col p-10 fixed justify-center items-end bg-white rounded-md">
-        <button onClick={handleModalCloseClick}>X</button>
+      <div className="flex w-4/5 flex-col p-10 pt-5 fixed justify-center items-end bg-white rounded-md">
+        <button className="text-2xl" onClick={handleModalCloseClick}>
+          X
+        </button>
         <img className="w-full" src={imageUrl} alt="" />
       </div>
     </div>
