3430c7631592ef00ce1805a5493c7c0b236b66ca
ba0061@naver.com <ba0061@naver.com>
2023-09-28 19:12:48 +0900

    feat: Pretendard 폰트 추가
    os마다 보여지는 폰트가 달라서 스타일 적용에 문제가 있었습니다.
    해결하고자 figma에서 현재 사용 중인 Pretendard 폰트 추가해주었습니다.
    normal, bold체에 대해서만 현재 적용해두었습니다.
    
    일단은 폰트 weight별로 9개 전부 commit 후,
    프로젝트 완성 시 사용치 않는 폰트는 삭제하겠습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{html,js}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Pretendard', 'sans-serif'],
+    }
   },
   plugins: [],
 };
