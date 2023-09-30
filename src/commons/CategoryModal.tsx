b032c5763c79897f69b7219b11f86f90aebfe3e8
ba0061@naver.com <ba0061@naver.com>
2023-09-30 21:46:14 +0900

    feat: 모달 css 변경

--- src/commons/CategoryModal.tsx
+++ src/commons/CategoryModal.tsx
@@ -55,8 +55,8 @@ const CategoryModal = ({
       onClick={handleModalOutsideClick}
       className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
     >
-      <div className="flex flex-col w-4/5 p-10 pt-5 fixed bg-white rounded-md">
-        <div className="flex justify-between">
+      <div className="flex flex-col w-3/5 h-4/5 fixed bg-white rounded-md overflow-auto">
+        <div className="flex justify-between p-5">
           <div className="flex gap-3">
             <button
               className={`${
@@ -81,13 +81,17 @@ const CategoryModal = ({
             X
           </button>
         </div>
+
+        {/* 밑 부분은 하나의 컴포넌트로 분리 가능해보임.
+            위에도 V컴포넌트로 분리 될 것 같긴 한데 굳이? */}
         {speciesOrRegion === 'species' && (
-          <div className="flex flex-col">
+          <div className="grid grid-cols-1 justify-items-center content-around h-full md:grid-cols-2">
             {speciesList.map((species) => {
               return (
                 <button
                   onClick={() => handleSpeciesClick(species)}
                   key={species}
+                  className="w-40 h-40 md:w-52 md:h-52 border rounded-md hover:bg-gray-100"
                 >
                   {species}
                 </button>
@@ -99,7 +103,11 @@ const CategoryModal = ({
           <div className="flex flex-col">
             {regionList.map((region) => {
               return (
-                <button onClick={() => handleRegionClick(region)} key={region}>
+                <button
+                  onClick={() => handleRegionClick(region)}
+                  key={region}
+                  className="flex flex-col justify-start pl-5 border border-b-white py-3 hover:bg-gray-100 hover:pl-6"
+                >
                   {region}
                 </button>
               );
