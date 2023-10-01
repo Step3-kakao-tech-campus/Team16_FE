57b06864860ba43b26ad1ef2130fd139a502e5ca
ba0061@naver.com <ba0061@naver.com>
2023-10-02 00:56:26 +0900

    feat: 상세정보 페이지에 임시로 gnb 적용

--- src/pages/DetailPetPage.tsx
+++ src/pages/DetailPetPage.tsx
@@ -1,8 +1,10 @@
 import DetailPetData from 'components/organisms/DetailPetData';
+import GNB from 'layouts/GNB';
 
 const DetailPetPage = () => {
   return (
     <div>
+      <GNB />
       <DetailPetData />
     </div>
   );
