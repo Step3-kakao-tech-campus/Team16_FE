41920516d4b0872ec9a992a7e1dc0250e67a9c61
ba0061@naver.com <ba0061@naver.com>
2023-10-01 17:36:50 +0900

    fix: 이미지 모달 상단과 하단이 잘리던 현상 고침

--- src/commons/ImageModal.tsx
+++ src/commons/ImageModal.tsx
@@ -14,13 +14,13 @@ const ImageModal = ({
       onClick={handleModalOutsideClick}
       className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
     >
-      <div className="flex w-4/5 max-w-6xl flex-col p-10 pt-5 fixed justify-center items-center bg-white rounded-md">
-        <div className="w-full flex justify-end">
+      <div className="w-4/5 h-3/5 md:h-4/5 overflow-scroll p-10 pt-5 fixed bg-white rounded-md">
+        <div className="w-full ">
           <button className="text-2xl" onClick={handleModalCloseClick}>
             X
           </button>
         </div>
-        <img className="w-4/5" src={imageUrl} alt="" />
+        <img className="w-4/5 mx-auto" src={imageUrl} alt="" />
       </div>
     </div>
   );
