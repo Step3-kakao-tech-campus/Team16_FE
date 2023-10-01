f440a636dd19a1e3a081a191b0c61e4b494379db
ba0061@naver.com <ba0061@naver.com>
2023-10-01 16:06:45 +0900

    feat: 모달 크기 변경

--- src/commons/ImageModal.tsx
+++ src/commons/ImageModal.tsx
@@ -14,11 +14,13 @@ const ImageModal = ({
       onClick={handleModalOutsideClick}
       className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
     >
-      <div className="flex w-4/5 flex-col p-10 pt-5 fixed justify-center items-end bg-white rounded-md">
-        <button className="text-2xl" onClick={handleModalCloseClick}>
-          X
-        </button>
-        <img className="w-full" src={imageUrl} alt="" />
+      <div className="flex w-4/5 flex-col p-10 pt-5 fixed justify-center items-center bg-white rounded-md">
+        <div className="w-full flex justify-end">
+          <button className="text-2xl" onClick={handleModalCloseClick}>
+            X
+          </button>
+        </div>
+        <img className="w-4/5" src={imageUrl} alt="" />
       </div>
     </div>
   );
