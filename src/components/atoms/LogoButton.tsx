e9f5c7facd40ea2d9cb34e9b758589c0151ade55
ba0061@naver.com <ba0061@naver.com>
2023-10-03 16:54:36 +0900

    refactor: paw-logo.png -> paw.png 이름 변경

--- src/components/atoms/LogoButton.tsx
+++ src/components/atoms/LogoButton.tsx
@@ -4,11 +4,7 @@ const LogoButton = () => {
   return (
     <Link to="/">
       <div className="flex gap-2">
-        <img
-          className="w-8"
-          src="/assets/images/paw-logo.png"
-          alt="애니모리 로고"
-        />
+        <img className="w-8" src="/assets/images/paw.png" alt="애니모리 로고" />
         <h1 className="text-brand-color font-bold text-2xl">애니모리</h1>
       </div>
     </Link>
