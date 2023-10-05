741d15f369234cb513a14beef90e2a28f17bff92
ba0061@naver.com <ba0061@naver.com>
2023-10-06 01:15:23 +0900

    feat: 사진, 동영상으로 작동하는 input과 버튼 완성
    pet 상세 정보는 recoil의 registerState 사용할 예정

---
+++ src/components/molecules/PictureVideoInput.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+
+interface PetPostProps {
+  name: string;
+  age: string;
+  type: string;
+  weight: number;
+  size: string;
+  sex: string;
+  vaccinationStatus: string;
+  adoptionStatus: boolean;
+  neutralizationStatus: boolean;
+  description: string;
+  petPolygonProfileDto: {
+    intelligence: number;
+    affinity: number;
+    athletic: number;
+    adaptability: number;
+    activeness: number;
+  };
+}
+
+const mockPetData: PetPostProps = {
+  name: '이렇게 긴 문장도 받을 수 있나요? 얼마나 긴 문장까지 자바에서 받을 수 있을까요? 이렇게 긴 문장도 받을 수 있나요? 얼마나 긴 문장까지 자바에서 받을 수 있을까요? 이렇게 긴 문장도 받을 수 있나요? 얼마나 긴 문장까지 자바에서 받을 수 있을까요? 이렇게 긴 문장도 받을 수 있나요? 얼마나 긴 문장까지 자바에서 받을 수 있을까요? 이렇게 긴 문장도 받을 수 있나요? 얼마나 긴 문장까지 자바에서 받을 수 있을까요? ',
+  age: '1',
+  type: 'DOG',
+  weight: 1,
+  size: '제가 보낸 요청',
+  sex: 'string',
+  vaccinationStatus: '안녕하세요',
+  adoptionStatus: false,
+  neutralizationStatus: false,
+  description: '잘 받아졌을까요',
+  petPolygonProfileDto: {
+    intelligence: 1,
+    affinity: 1,
+    athletic: 1,
+    adaptability: 1,
+    activeness: 1,
+  },
+};
+
+const ImageVideoInput = () => {
+  const [selectedImageFile, setSelectedImageFile] = useState(null);
+  const [selectedVideoFile, setSelectedVideoFile] = useState(null);
+
+  const { mutate } = useMutation(async (formData: FormData): Promise<any> => {
+    const res = await fetch(
+      'http://ec2-3-37-14-140.ap-northeast-2.compute.amazonaws.com/api/pet',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    );
+    const data = await res.json();
+    return data;
+  });
+
+  const handleUploadButtonClick = async () => {
+    if (!selectedImageFile || !selectedVideoFile) return;
+    console.log(selectedImageFile, selectedVideoFile, mockPetData);
+
+    const formData = new FormData();
+    formData.append('video', selectedVideoFile);
+    formData.append('image', selectedImageFile);
+    formData.append(
+      'key',
+      new Blob([JSON.stringify(mockPetData)], {
+        type: 'application/json',
+      }),
+    );
+
+    const data = await mutate(formData);
+    console.log(data);
+  };
+
+  const handleChange = (e: any) => {
+    if (e.target.files[0].type.includes('image')) {
+      setSelectedImageFile(e.target.files[0]);
+    } else if (e.target.files[0].type.includes('video')) {
+      setSelectedVideoFile(e.target.files[0]);
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <button onClick={handleUploadButtonClick}>확인</button>
+      <div>
+        <input type="file" onChange={handleChange} accept="image/*" />
+        <input type="file" onChange={handleChange} accept="video/*" />
+      </div>
+    </div>
+  );
+};
+
+export default ImageVideoInput;
