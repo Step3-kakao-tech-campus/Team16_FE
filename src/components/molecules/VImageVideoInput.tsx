aae241ce4dfb3a4613216a71910a2f3ba32494bf
ba0061@naver.com <ba0061@naver.com>
2023-10-07 01:33:37 +0900

    style: 동영상과 사진 업로드 위치 변경

--- src/components/molecules/VImageVideoInput.tsx
+++ src/components/molecules/VImageVideoInput.tsx
@@ -27,20 +27,20 @@ const VImageVideoInput = (props: VImageVideoInputProps) => {
       <FileInput
         handleInputChange={handleInputChange}
         handleCustomButtonClick={handleCustomButtonClick}
-        fileRef={imageRef}
-        isFileSelected={isImageSelected}
-        fileType="image"
-        labelText="애니모리 친구들 프로필에 올라갈 사진을 업로드해주세요."
-        uploadDescription="최대 5MB"
-      />
-      <FileInput
-        handleInputChange={handleInputChange}
-        handleCustomButtonClick={handleCustomButtonClick}
         fileRef={videoRef}
         isFileSelected={isVideoSelected}
         fileType="video"
         labelText="애니모리의 숏폼에 올라갈 짧은 동영상을 업로드해주세요."
         uploadDescription="1 ~ 20초 분량, 최대 15MB"
+      />
+      <FileInput
+        handleInputChange={handleInputChange}
+        handleCustomButtonClick={handleCustomButtonClick}
+        fileRef={imageRef}
+        isFileSelected={isImageSelected}
+        fileType="image"
+        labelText="애니모리 친구들 프로필에 올라갈 사진을 업로드해주세요."
+        uploadDescription="최대 5MB"
       />
     </div>
   );
