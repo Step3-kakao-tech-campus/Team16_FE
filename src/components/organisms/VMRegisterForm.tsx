e5d022c905c7f2d7cd1dfbeb44d2fd05fd745ab5
LimSumi <thddkwl5330@naver.com>
2023-10-07 23:32:45 +0900

    feat: year, month 나누기 시도

--- src/components/organisms/VMRegisterForm.tsx
+++ src/components/organisms/VMRegisterForm.tsx
@@ -11,27 +11,50 @@ const MRegisterForm = ({ handleChange }: RegisterProps) => {
     <div className="flex flex-col items-center gap-8 mt-10">
       <div className="flex  justify-center sm:gap-20 w-1/2 text-sm  mb-5">
         <div className=" flex-col sm:flex-row flex gap-5 w-full ">
-          <InputGroup
-            id="name"
-            name="이름"
-            type="text"
-            placeholder="이름을 입력해주세요"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            autocomplete="name"
-          />
-
-          <InputGroup
-            id="age"
-            name="나이"
-            type="number"
-            placeholder="연도"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            autocomplete="age"
-          />
+          <div className="">
+            <InputGroup
+              id="name"
+              name="이름"
+              type="text"
+              placeholder="이름을 입력해주세요"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              autocomplete="name"
+            />
+          </div>
+          <div className="flex w-2/4 whitespace-nowrap">
+            <div className=" w-2/6">
+              <InputGroup
+                id="year"
+                name="나이"
+                type="number"
+                placeholder="연도"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                autocomplete="age"
+              />
+            </div>
+            <text className="flex h-14 items-end font-semibold whitespace-nowrap">
+              년도
+            </text>
+            <div className=" w-2/6">
+              <InputGroup
+                id="month"
+                name="ㅤ"
+                type="number"
+                placeholder="개월"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                autocomplete="age"
+              />
+            </div>
+            <text className="flex h-14 items-end font-semibold whitespace-nowrap">
+              개월
+            </text>
+          </div>
           <SelectBox />
         </div>
       </div>
@@ -77,6 +100,20 @@ const MRegisterForm = ({ handleChange }: RegisterProps) => {
               handleChange(e);
             }}
             autocomplete="vaccinationStatus"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center gap-5 sm:gap-20 w-1/2 text-sm  mb-5">
+        <div className="w-full">
+          <InputGroup
+            id="description"
+            name="상세설명"
+            type="text"
+            placeholder="상세 설명을 입력해주세요"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            autocomplete="description"
           />
         </div>
       </div>
