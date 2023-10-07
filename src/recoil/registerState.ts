764e23e5d7b7d919cee05d6e85816bf67eb6e9aa
LimSumi <thddkwl5330@naver.com>
2023-10-07 22:25:03 +0900

    feat: registerState 해결

--- src/recoil/registerState.ts
+++ src/recoil/registerState.ts
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 export interface RegisterType {
   name: string;
   age: string; // 0년2개월 ("x년y개월")
-  type: string; // DOG | CAT | ETC
+  type: string; // DOG | CAT
   weight: number; // float 허용 6.52 (단위:kg)
   size: string; // String
   sex: string; // MALE | FEMALE
