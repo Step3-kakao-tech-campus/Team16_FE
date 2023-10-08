bd81a02f1e690ccd7e8e3324089cb127dfe0f500
LimSumi <thddkwl5330@naver.com>
2023-10-08 21:28:09 +0900

    refactor: VCategoryModalList 위치 변경

--- src/commons/modals/CategoryModal.tsx
+++ src/commons/modals/CategoryModal.tsx
@@ -4,7 +4,7 @@ import speciesState, { SpeciesType } from 'recoil/speciesState';
 import CategoryModalList, {
   CategoryModalType,
   VCategoryModalListProps,
-} from 'commons/VCategoryModalList';
+} from 'commons/modals/VCategoryModalList';
 
 export interface CategoryModalProps {
   handleModalCloseClick: () => void;
