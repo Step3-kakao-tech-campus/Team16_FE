5d51c02e04fd71eeaa2e48eb287d8d99e9467022
ba0061@naver.com <ba0061@naver.com>
2023-10-09 19:56:31 +0900

    feat: api와 실제 연결 확인
    headers에JWT 토큰 전송
    임시적으로 isLoading 처리
    오각형 차트에 필요한 데이터가 petPolygonProfileDto에 있음을 확인
    mockDetailPetInfo 삭제

--- src/pages/detailPet/DetailPetData.tsx
+++ src/pages/detailPet/DetailPetData.tsx
@@ -13,63 +13,30 @@ const DetailPetData = () => {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [modal, setModal] = useState(false);
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['pet', petId],
     queryFn: () => {
       return fetch(`${process.env.REACT_APP_URI}/pet/${petId}`).then((res) =>
         res.json(),
       );
     },
-    onSuccess: (res) => {
-      console.log(res);
-    },
-    onError: (err) => {
-      console.log(err);
-    },
   });
-
+  if (isLoading) return <div>로딩중</div>;
   const labels = ['귀여움', '침착함', '유머감각', '외모', '의젓함'];
 
-  const mockDetailPetInfo: MockDetailPetInfoProps = {
-    shelterInfo: {
-      id: 1,
-      name: '광주어찌고보호소',
-      contact: '010-2312-2146',
-    },
-    name: '뽀삐',
-    age: '3살',
-    sex: 'Male',
-    weight: 5,
-    description: StringWithLineBreak(
-      '뽀삐는 귀여움이 넘치는 강아지입니다. \n착하게 말도 잘 듣고요\n자신의 일을 묵묵히 하는 타입입니다.\n다 크면 10kg 넘을 듯해요.\n개농장에서구조했슴',
-    ),
-    protectionExpirationDate: '2023-10-25',
-    vaccinationStatus: 'YES',
-    neutralizationStatus: 'YES',
-    adoptionStatus: 'NO',
-    profileImageUrl: '/assets/images/logo512.png',
-    size: '수박만함',
-    polygonProfile: {
-      intelligence: 1,
-      affinity: 3,
-      athletic: 4,
-      adaptability: 3,
-      activeness: 2,
-    },
-  };
   const radarChartProps: RadarChartProps = {
     setCanvas,
     width: 400,
     height: 400,
     canvas,
     labels,
-    data: mockDetailPetInfo.polygonProfile,
+    data: data.response.petPolygonProfileDto,
     willAnimate: true,
   };
 
   const vDetailPetDataProps = {
     radarChartProps,
-    mockDetailPetInfoProps: mockDetailPetInfo,
+    mockDetailPetInfoProps: data.response,
     modal,
     handleModalImageClick: () => setModal(true),
     handleModalCloseClick: () => setModal(false),
@@ -78,7 +45,7 @@ const DetailPetData = () => {
         setModal(false);
       }
     },
-    imageUrl: mockDetailPetInfo.profileImageUrl,
+    imageUrl: data.response.profileImageUrl,
   };
 
   return <VDetailPetData {...vDetailPetDataProps} />;
