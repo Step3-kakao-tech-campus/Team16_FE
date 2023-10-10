99a82c1d65ea37370fa1fa83766cd432cc3485b7
hjiwon <ba0061@naver.com>
2023-10-11 07:53:35 +0900

    fix: isError, isLoading, isSuccess 상황에 맞게 모달 변경
    기존 겹치던 문제 해결
    
    i Please enter the commit message for your changes. Lines starting

--- src/commons/modals/RegisterModal.tsx
+++ src/commons/modals/RegisterModal.tsx
@@ -19,71 +19,118 @@ const RegisterModal = ({
   isSuccess,
   isError,
 }: RegisterModalProps) => {
-  return (
-    <div
-      onClick={handleModalOutsideClick}
-      className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
-    >
-      <div className="w-80 h-60 bg-white rounded-md flex flex-col items-center">
-        <div className="flex w-full justify-end p-5 ">
-          <button onClick={handleModalCloseClick} className="text-2xl">
-            X
-          </button>
+  if (isError) {
+    return (
+      <div
+        onClick={handleModalOutsideClick}
+        className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <div className="w-80 h-60 bg-white rounded-md flex flex-col items-center">
+          <div className="flex w-full justify-end p-5 ">
+            <button onClick={handleModalCloseClick} className="text-2xl">
+              X
+            </button>
+          </div>
+          <span className="text-2xl text-brand-color">등록에 실패했습니다</span>
+          <div className="flex w-2/3 justify-between mt-8">
+            <button
+              className="bg-brand-color rounded-md font-bold text-white w-16 py-2"
+              onClick={handleRegisterMoreButtonClick}
+            >
+              확인
+            </button>
+          </div>
         </div>
-        {isSuccess && (
-          <>
-            <span className="text-2xl text-brand-color">
-              추가 등록 하시겠습니까?
-            </span>
-            <div className="flex w-2/3 justify-between mt-8">
-              <button
-                className="text-brand-color rounded-md font-bold border border-brand-color w-16 py-2"
-                onClick={handleRegisterFinishButtonClick}
-              >
-                아니요
-              </button>
-              <button
-                className="bg-brand-color rounded-md font-bold text-white w-16 py-2"
-                onClick={handleRegisterMoreButtonClick}
-              >
-                예
-              </button>
-            </div>
-          </>
-        )}
-        {!isSuccess && isLoading ? (
-          <>
-            <span className="text-2xl text-brand-color">등록중입니다...</span>
-            <img
-              src="assets/images/hourglass.png"
-              alt="hourglass"
-              className="w-10 h-10 mt-8"
-            />
-          </>
-        ) : (
-          <>
-            <span className="text-2xl text-brand-color">
-              등록 하시겠습니까?
-            </span>
-            <div className="flex w-2/3 justify-between mt-8">
-              <button
-                className="text-brand-color rounded-md font-bold border border-brand-color w-16 py-2"
-                onClick={handleModalCloseClick}
-              >
-                아니요
-              </button>
-              <button
-                className="bg-brand-color rounded-md font-bold text-white w-16 py-2"
-                onClick={handleRegisterButtonClick}
-              >
-                예
-              </button>
-            </div>
-          </>
-        )}
       </div>
-    </div>
-  );
+    );
+  }
+  if (isLoading) {
+    return (
+      <div
+        onClick={handleModalOutsideClick}
+        className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <div className="w-80 h-60 bg-white rounded-md flex flex-col items-center">
+          <div className="flex w-full justify-end p-5 ">
+            <button onClick={handleModalCloseClick} className="text-2xl">
+              X
+            </button>
+          </div>
+          <span className="text-2xl text-brand-color">등록중입니다...</span>
+          <img
+            src="assets/images/hourglass.png"
+            alt="hourglass"
+            className="w-10 h-10 mt-8"
+          />
+        </div>
+      </div>
+    );
+  }
+  if (isSuccess) {
+    return (
+      <div
+        onClick={handleModalOutsideClick}
+        className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <div className="w-80 h-60 bg-white rounded-md flex flex-col items-center">
+          <div className="flex w-full justify-end p-5 ">
+            <button onClick={handleModalCloseClick} className="text-2xl">
+              X
+            </button>
+          </div>
+          <span className="text-2xl text-brand-color">
+            추가 등록 하시겠습니까?
+          </span>
+          <div className="flex w-2/3 justify-between mt-8">
+            <button
+              className="text-brand-color rounded-md font-bold border border-brand-color w-16 py-2"
+              onClick={handleRegisterFinishButtonClick}
+            >
+              아니요
+            </button>
+            <button
+              className="bg-brand-color rounded-md font-bold text-white w-16 py-2"
+              onClick={handleRegisterMoreButtonClick}
+            >
+              예
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (!isSuccess && !isLoading && !isError) {
+    return (
+      <div
+        onClick={handleModalOutsideClick}
+        className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <div className="w-80 h-60 bg-white rounded-md flex flex-col items-center">
+          <div className="flex w-full justify-end p-5 ">
+            <button onClick={handleModalCloseClick} className="text-2xl">
+              X
+            </button>
+          </div>
+          <span className="text-2xl text-brand-color">등록 하시겠습니까?</span>
+          <div className="flex w-2/3 justify-between mt-8">
+            <button
+              className="text-brand-color rounded-md font-bold border border-brand-color w-16 py-2"
+              onClick={handleModalCloseClick}
+            >
+              아니요
+            </button>
+            <button
+              className="bg-brand-color rounded-md font-bold text-white w-16 py-2"
+              onClick={handleRegisterButtonClick}
+            >
+              예
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <div>문제가 생겼습니다</div>;
 };
 
 export default RegisterModal;
