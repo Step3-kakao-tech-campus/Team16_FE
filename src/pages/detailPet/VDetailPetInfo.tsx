0e1bb6b99b5fad89a27765e10f41635b216271cd
hjiwon <ba0061@naver.com>
2023-10-11 11:21:33 +0900

    feat: 이름과 상세설명 width 값 추가
    5/6

--- src/pages/detailPet/VDetailPetInfo.tsx
+++ src/pages/detailPet/VDetailPetInfo.tsx
@@ -3,8 +3,8 @@ import { DetailPetInfoProps } from 'pages/detailPet/VDetailPetData';
 
 const VDetailPetInfo = (data: DetailPetInfoProps) => {
   return (
-    <div>
-      <h2 className="text-3xl mb-10">{data.name}</h2>
+    <div className="flex flex-col items-center">
+      <h2 className="text-3xl w-5/6 mb-10">{data.name}</h2>
       <div className="flex flex-col items-center">
         <div className="flex h-14 justify-around">
           <div className="flex">
@@ -74,7 +74,7 @@ const VDetailPetInfo = (data: DetailPetInfoProps) => {
           </span>
         </a>
 
-        <div className="w-full">{data.description}</div>
+        <div className="w-5/6">{data.description}</div>
       </div>
     </div>
   );
