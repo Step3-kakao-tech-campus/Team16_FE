d083c41b0337374ddc57eb95d3fbf4a3b9cab7ca
LimSumi <thddkwl5330@naver.com>
2023-10-14 03:32:13 +0900

    fix: 프로필 리스트 홈, 긴급, 신규 ui 수정
    margin 수정 및 가운데 정렬, 더보기 버튼 위치 수정

--- src/pages/profileList/VProfileListHome.tsx
+++ src/pages/profileList/VProfileListHome.tsx
@@ -11,32 +11,32 @@ export interface ProfileListProps {
 
 const VProfileListHome = (profileListProps: ProfileListProps) => {
   return (
-    <div className="m-20">
-      <h2 className="font-bold text-xl sm:text-2xl m-10 mr-20  whitespace-nowrap">
+    <div className="mx-16 sm:mx-72 sm:my-20">
+      <h2 className="flex w-full font-bold text-xl sm:text-2xl justify-center items-center whitespace-nowrap">
         긴급 도움이 필요해요!
         <a
           href="profile/urgent/1"
-          className="font-normal text-sm flex sm:float-right mt-3 sm:mt-0 text-gray-600"
+          className="font-normal w-full text-right justify-end text-sm flex sm:float-right text-gray-600"
         >
           더보기
         </a>
       </h2>
-      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 m-10">
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 my-10 w-full">
         <ProfileCard {...profileListProps} />
         <ProfileCard {...profileListProps} />
         <ProfileCard {...profileListProps} />
         <ProfileCard {...profileListProps} />
       </div>
-      <h2 className="font-bold text-xl sm:text-2xl m-10 mr-20  whitespace-nowrap">
+      <h2 className="flex w-full font-bold text-xl sm:text-2xl justify-center items-center whitespace-nowrap">
         신규 애니모리 친구들
         <a
           href="/profile/new/1"
-          className="font-normal text-sm flex sm:float-right mt-3 sm:mt-0 text-gray-600"
+          className="font-normal w-full text-right justify-end text-sm flex sm:float-right text-gray-600"
         >
           더보기
         </a>
       </h2>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 m-10">
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 my-10 w-full">
         <ProfileCard {...profileListProps} />
         <ProfileCard {...profileListProps} />
         <ProfileCard {...profileListProps} />
