2dec70cd84cf03b8f6bbfa4f75a21b52fd5d8b0a
hjiwon <ba0061@naver.com>
2023-10-15 21:17:44 +0900

    feat: 권한없음 fallback 추가

--- src/pages/update/UpdatePage.tsx
+++ src/pages/update/UpdatePage.tsx
@@ -7,7 +7,7 @@ const UpdatePage = () => {
   return (
     <>
       <GNB />
-      <ErrorBoundary>
+      <ErrorBoundary fallback={<div className="">권한이 없습니다.</div>}>
         <Suspense fallback={<div className="h-screen w-screen bg-slate-500" />}>
           <UpdateTemplate />
         </Suspense>
