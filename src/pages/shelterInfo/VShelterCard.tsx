c2f93c87eebc582a7fec87521bd89d7b8b090cbc
LimSumi <thddkwl5330@naver.com>
2023-10-22 07:14:06 +0900

    feat: 보호소 프로필 api 연결

--- src/pages/shelterInfo/VShelterCard.tsx
+++ src/pages/shelterInfo/VShelterCard.tsx
@@ -6,23 +6,22 @@ interface VShelterInfoProps {
 }
 
 const VShelterCard = ({ name, id, adress, call }: VShelterInfoProps) => (
-  <div className="flex-initial m-20">
-    <li className="block ml-14">
-      <a href="#" className="relative block group">
-        <img src={'/assets/frame.png'} alt="" className="object-cover w-auto" />
-
-        <div className="absolute text-sm sm:text-lg font-bold inset-0 flex flex-col items-start justify-start p-6">
-          <div className="text-xl font-Bold">{name}</div>
-          <div className="mt-1.5 max-w-[40ch] font-medium center">{adress}</div>
-          <div className="mt-1.5 mb-1 max-w-[40ch] font-medium">
-            연락처 {call}
+  <div className="flex w-auto justify-center">
+    <div className="flex mx-2">
+      <div className="relative flex">
+        <img src={'/assets/frame.png'} alt="" className=" w-auto" />
+        <div className="absolute text-xs sm:text-lg inset-0 flex flex-col items-start justify-start px-8 py-5 sm:p-10">
+          <div className="flex text-lg sm:text-2xl font-bold whitespace-nowrap">
+            {name}
           </div>
-          <span className="mt-auto inline-block bg-green-700 px-2 py-1.5 text-xs font-medium uppercase tracking-wide text-white rounded-xl">
-            전화걸기
+          <div className="flex center whitespace-nowrap">{adress}</div>
+          <div className="sm:mt-3 mb-1 whitespace-nowrap">연락처 {call}</div>
+          <span className="mt-auto inline-block bg-green-700 px-2 py-1 text-xs font-medium uppercase text-white rounded-xl whitespace-nowrap">
+            <a href={`tel:${call}`}>전화하기</a>
           </span>
         </div>
-      </a>
-    </li>
+      </div>
+    </div>
   </div>
 );
 
