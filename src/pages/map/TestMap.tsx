f6f8060bda55d11607ac1321fd4cbded4ef6d9ad
JeonDoGyun <wjsehrbs2632@naver.com>
2023-10-25 13:26:00 +0900

    feat: 키워드 검색 후 필터링 구현
    키워드 검색으로 장소 가져오도록 했습니다.
    리스트화를 생각해서 주소, 이름, 좌표를 필터링하여 가져오도록 했습니다.

--- src/pages/map/TestMap.tsx
+++ src/pages/map/TestMap.tsx
@@ -1,48 +1,70 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import useKakaoLoader from './useKakaoLoader';
 
 type Coordinate = {
   lat: number;
   lng: number;
 };
 
-interface SearchedPlaceType {
-  position: { lat: number; lng: number };
-  content: string;
-}
-
 type PlaceType = {
   address_name: string;
-  distance: number;
+  category_group_code: string;
+  category_group_name: string;
+  category_name: string;
+  distance: string;
   id: string;
   place_name: string;
   place_url: string;
   road_address_name: string;
-  x: number;
-  y: number;
+  x: string;
+  y: string;
+};
+
+// 보호소 키워드로 검색한 후 저장
+const searchKeywordPlace = (
+  state: PlaceType[],
+  setState: React.Dispatch<React.SetStateAction<PlaceType[]>>,
+) => {
+  const ps = new kakao.maps.services.Places();
+  ps.keywordSearch(
+    '동물 보호소',
+    (data: any, status: kakao.maps.services.Status) => {
+      if (status === kakao.maps.services.Status.OK && data) {
+        setState(data);
+      } else if (status === kakao.maps.services.Status.OK) {
+        // 검색 결과가 없을 때 -> 이 부분은 나중에 어떻게 표현할지 회의 필요
+        alert('검색 결과가 없습니다.');
+      }
+    },
+  );
+  // 여기서 필터링?
+  if (state.length > 0) {
+    const filteredSearchedPlaces = state.map((searchedPlace) => ({
+      address_name: searchedPlace.address_name,
+      place_name: searchedPlace.place_name,
+      lat: searchedPlace.x,
+      lng: searchedPlace.y,
+    }));
+    return filteredSearchedPlaces;
+  }
+  return [];
 };
 
 const TestMap = () => {
-  useKakaoLoader();
   const [currentPosition, setCurrentPosition] = useState<Coordinate>({
     lat: 35.1759293,
     lng: 126.9149701,
   });
-  const [markers, setMarkers] = useState<Array<SearchedPlaceType>>([]);
-  const [searchedPlaces, setSearchedPlaces] = useState<Array<object>>([]);
+  const [markers, setMarkers] = useState<Array<PlaceType>>([]);
+  const [searchedPlaces, setSearchedPlaces] = useState<Array<PlaceType>>([]);
 
-  // 키워드 검색
+  // 필요한 값만 새로운 객체로 저장
   useEffect(() => {
-    const ps = new kakao.maps.services.Places();
-    const places: any = [];
-    ps.keywordSearch('보호소', (data, status) => {
-      if (status === kakao.maps.services.Status.OK && data) {
-        places.push(data);
-      }
-      setSearchedPlaces(places);
-    });
-    console.log(searchedPlaces);
+    const filteredPlaces = searchKeywordPlace(
+      searchedPlaces,
+      setSearchedPlaces,
+    );
+    console.log('filteredPlace: ', filteredPlaces);
   }, []);
 
   return (
@@ -51,7 +73,6 @@ const TestMap = () => {
       center={currentPosition}
       className="w-[500px] h-[500px]"
       level={3}
-      onCreate={() => {}}
     >
       <MapMarker position={currentPosition}>현재 위치</MapMarker>
     </Map>
