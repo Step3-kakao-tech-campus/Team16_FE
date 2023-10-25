e493a472b9a79f680926e9ed07b7c88609103a68
JeonDoGyun <wjsehrbs2632@naver.com>
2023-10-26 00:24:51 +0900

    feat: 키워드 검색 결과 Marker로 지도에 표시
    키워드 검색 결과 리스트 가져오는 것까지 구현했고, MapMarker가 작동은 하는데 표시는 안되어서 이 부분 해결 중입니다.

--- src/pages/map/TestMap.tsx
+++ src/pages/map/TestMap.tsx
@@ -20,34 +20,11 @@ type PlaceType = {
   y: string;
 };
 
-// 보호소 키워드로 검색한 후 저장
-const searchKeywordPlace = (
-  state: PlaceType[],
-  setState: React.Dispatch<React.SetStateAction<PlaceType[]>>,
-) => {
-  const ps = new kakao.maps.services.Places();
-  ps.keywordSearch(
-    '동물 보호소',
-    (data: any, status: kakao.maps.services.Status) => {
-      if (status === kakao.maps.services.Status.OK && data) {
-        setState(data);
-      } else if (status === kakao.maps.services.Status.OK) {
-        // 검색 결과가 없을 때 -> 이 부분은 나중에 어떻게 표현할지 회의 필요
-        alert('검색 결과가 없습니다.');
-      }
-    },
-  );
-  // 여기서 필터링?
-  if (state.length > 0) {
-    const filteredSearchedPlaces = state.map((searchedPlace) => ({
-      address_name: searchedPlace.address_name,
-      place_name: searchedPlace.place_name,
-      lat: searchedPlace.x,
-      lng: searchedPlace.y,
-    }));
-    return filteredSearchedPlaces;
-  }
-  return [];
+type FilteredPlaceType = {
+  address_name: string;
+  place_name: string;
+  lat: string;
+  lng: string;
 };
 
 const TestMap = () => {
@@ -57,25 +34,90 @@ const TestMap = () => {
   });
   const [markers, setMarkers] = useState<Array<PlaceType>>([]);
   const [searchedPlaces, setSearchedPlaces] = useState<Array<PlaceType>>([]);
+  const [isExecuted, setIsExecuted] = useState<boolean>(false);
+
+  // 보호소 키워드로 검색한 후 저장
+  const searchKeywordPlace = (
+    state: PlaceType[],
+    setState: React.Dispatch<React.SetStateAction<PlaceType[]>>,
+  ) => {
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(
+      '동물 보호소',
+      (data: any, status: kakao.maps.services.Status) => {
+        if (status === kakao.maps.services.Status.OK && data) {
+          setState(data);
+          setIsExecuted(true);
+        } else if (status === kakao.maps.services.Status.ERROR) {
+          // 검색 결과가 없을 때 -> 이 부분은 나중에 어떻게 표현할지 회의 필요
+          alert('검색 결과가 없습니다.');
+          setIsExecuted(false);
+        }
+      },
+    );
+  };
 
   // 필요한 값만 새로운 객체로 저장
   useEffect(() => {
-    const filteredPlaces = searchKeywordPlace(
-      searchedPlaces,
-      setSearchedPlaces,
-    );
-    console.log('filteredPlace: ', filteredPlaces);
-  }, []);
+    if (!isExecuted) searchKeywordPlace(searchedPlaces, setSearchedPlaces);
+    console.log('filteredPlaces: ', searchedPlaces);
+  }, [searchedPlaces]);
 
   return (
-    <Map
-      id="map"
-      center={currentPosition}
-      className="w-[500px] h-[500px]"
-      level={3}
-    >
-      <MapMarker position={currentPosition}>현재 위치</MapMarker>
-    </Map>
+    <div className="flex">
+      <Map
+        id="map"
+        center={currentPosition}
+        className="w-[500px] h-[500px]"
+        level={3}
+      >
+        {/* <MapMarker
+          position={currentPosition}
+          image={{
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
+            size: {
+              width: 24,
+              height: 35,
+            }, // 마커이미지의 크기입니다
+          }}
+          title="현재 위치"
+        /> */}
+        {/* Marker 표시 */}
+        {searchedPlaces.map((searchedPlace) => {
+          console.log('마커 작동여부 확인');
+          return (
+            <MapMarker
+              key={`${searchedPlace.id}-${searchedPlace.place_name}`}
+              position={{
+                x: Number(searchedPlace.x),
+                y: Number(searchedPlace.y),
+              }} // 마커를 표시할 위치
+              image={{
+                src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
+                size: {
+                  width: 24,
+                  height: 35,
+                }, // 마커이미지의 크기입니다
+              }}
+              title={searchedPlace.place_name} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            ></MapMarker>
+          );
+        })}
+      </Map>
+      <div className="list flex flex-col justify-center">
+        {isExecuted && searchedPlaces.length > 0 ? (
+          searchedPlaces.map((searchedPlace, index) => (
+            <span key={index} className=" border-gray-200 ">
+              <span className="">{searchedPlace.place_name}</span>
+              <span className="">{searchedPlace.address_name}</span>
+            </span>
+          ))
+        ) : (
+          <div className="border-gray-200">검색 결과가 없습니다.</div>
+        )}
+      </div>
+    </div>
   );
 };
 
