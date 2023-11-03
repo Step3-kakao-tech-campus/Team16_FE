4f495aecec3098915615ab9e592f795a60ed4b7b
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-03 23:12:28 +0900

    fix: 유저 Selectbox의 로그아웃 시 로그인 페이지로 리다이렉트 되는 부분 수정
    로그아웃 시 로그인 페이지로 이동하는 것이 부자연스러워서
    페이지 이동 없이  토큰만 제거하는 것으로 수정

--- src/commons/UserDropdownBox.tsx
+++ src/commons/UserDropdownBox.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { getCookie, removeCookie } from './cookie/cookie';
+import { getCookie, removeCookie, setCookie } from './cookie/cookie';
 
 // 로그인 되었을 때 상태를 보여주는 SelectBox 제작
 const UserDropdownBox = () => {
@@ -30,7 +30,8 @@ const UserDropdownBox = () => {
         break;
       case '로그아웃':
         removeToken();
-        navigate('/login');
+        window.location.reload();
+        setCookie('userAccountInfo', 'Not Login');
         break;
       default:
         break;
