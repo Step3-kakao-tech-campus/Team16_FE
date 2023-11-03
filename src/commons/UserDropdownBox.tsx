90b85dcb3915ddc80bd98b533ab2834af11c0936
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-03 23:06:26 +0900

    feat: 로그인 상태 유저 정보 Selectbox 구현
    로그인 상태일 때 페이지 리다이렉트 기능을 가진 Selectbox 구현
    - My 정보변경 -> 유저 정보 변경 페이지
    - My 보호소 -> 보호소 정보 페이지
    - 로그아웃 -> 로그인 페이지

---
+++ src/commons/UserDropdownBox.tsx
@@ -0,0 +1,104 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { getCookie, removeCookie } from './cookie/cookie';
+
+// 로그인 되었을 때 상태를 보여주는 SelectBox 제작
+const UserDropdownBox = () => {
+  const token = getCookie('loginToken');
+  const options = ['My 정보 변경', 'My 보호소 이동', '로그아웃'];
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 메뉴 열림/닫힘 상태
+  const navigate = useNavigate();
+  const shelterId = getCookie('userAccountInfo');
+  const id = shelterId ? shelterId.split(' ')[1] : '';
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const removeToken = () => {
+    removeCookie('loginToken');
+    removeCookie('userAccountInfo');
+  };
+
+  const handleOptionClick = (option: string) => {
+    switch (option) {
+      case 'My 정보 변경':
+        navigate('/shelter/:id/edit'); // 아직 회원정보 수정 페이지 구현 안됨
+        break;
+      case 'My 보호소 이동':
+        navigate(`/shelter/${id}/1`);
+        break;
+      case '로그아웃':
+        removeToken();
+        navigate('/login');
+        break;
+      default:
+        break;
+    }
+    setIsDropdownOpen(false);
+  };
+
+  if (!token) {
+    return (
+      <div className="flex gap-4 font-bold">
+        <button
+          className="border-2 box-content border-brand-color text-brand-color rounded-md py-1 px-4 transition duration-300 hover:bg-brand-color hover:text-white"
+          onClick={() => navigate('/login')}
+        >
+          로그인
+        </button>
+        <button
+          className="border-brand-color border-2 bg-brand-color text-white rounded-md py-1 px-4 transition duration-300 hover:bg-white hover:text-brand-color"
+          onClick={() => navigate('/signup')}
+        >
+          회원가입
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <button
+          onClick={toggleDropdown}
+          type="button"
+          className="inline-flex justify-center w-full rounded-full border-2 border-gray-300 hover:border-gray-400 shadow-sm p-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100"
+          id="options-menu"
+          aria-haspopup="listbox"
+          aria-expanded="true"
+        >
+          <img
+            src="/assets/images/shelterIcon.png"
+            alt="유저 정보 아이콘"
+            className="w-8 h-8"
+          />
+        </button>
+      </div>
+
+      {isDropdownOpen && (
+        <div className="origin-top-right absolute border-2 border-gray-300 hover:border-gray-400 right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            {options.map((option, index) => (
+              <div
+                key={index}
+                className="block px-4 py-2 text-sm font-bold text-gray-500 hover:bg-gray-100 hover:text-black cursor-pointer"
+                onClick={() => handleOptionClick(option)}
+                role="menuitem"
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserDropdownBox;
