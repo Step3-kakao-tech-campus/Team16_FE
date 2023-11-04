d54220ea9e6bb668fe17d49a608d29f991679f75
hjiwon <ba0061@naver.com>
2023-11-05 06:29:30 +0900

    feat: 에러 페이지에 로고 추가, 홈으로 가는 버튼 추가

--- src/commons/ErrorBoundary.tsx
+++ src/commons/ErrorBoundary.tsx
@@ -36,10 +36,21 @@ class ErrorBoundary extends Component<Props, State> {
       if (this.state.error?.message) {
         return (
           <div className="flex flex-col mt-12 gap-12 items-center justify-center w-screen">
+            <img
+              src="/assets/images/backgroundimage.png"
+              alt="backgroundimage"
+              className="w-60 h-60 object-cover"
+            />
             <h1 className="text-3xl font-bold text-brand-color">
               뭔가 문제가 생겼어요..
             </h1>
             <h2 className="text-2xl font-bold">{this.state.error.message}</h2>
+            <Link
+              to="/"
+              className="border flex items-center justify-center box-border border-brand-color border-2 bg-brand-color text-white rounded-md w-20 py-1 hover:bg-white hover:text-brand-color transition duration-300 ease-in-out"
+            >
+              홈으로
+            </Link>
           </div>
         );
       }
@@ -51,6 +62,12 @@ class ErrorBoundary extends Component<Props, State> {
           <h2 className="text-2xl font-bold">
             해결 중에 있습니다. 다시 한 번 시도해주세요!
           </h2>
+          <Link
+            to="/"
+            className="border flex items-center justify-center box-border border-brand-color border-2 bg-brand-color text-white rounded-md w-20 py-1 hover:bg-white hover:text-brand-color transition duration-300 ease-in-out"
+          >
+            홈으로
+          </Link>
         </div>
       ); // 에러 메시지도 없고 fallback도 없으면 그냥 에러 메시지 출력
     }
