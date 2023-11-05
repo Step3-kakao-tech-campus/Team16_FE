54b6c87e8eb0525497569b8624e5a71ea09849e0
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-05 21:38:48 +0900

    style: EditProfilePage 스켈레톤 추가
    별도의 Skeleton 파일을 만들어서 useQuery loading 시 Skeleton을 보여주도록 했습니다.

--- src/pages/editProfile/EditProfilePage.tsx
+++ src/pages/editProfile/EditProfilePage.tsx
@@ -9,6 +9,7 @@ import { shelterSignupState } from 'recoil/shelterState';
 import { useEffect, useState } from 'react';
 import { getCookie } from 'commons/cookie/cookie';
 import EditAddressInputGroup from './EditAddressInputGroup';
+import EditProfilePageSkeleton from './EditProfilePageSkeleton';
 
 const EditProfilePage = () => {
   const params = useParams();
@@ -99,50 +100,54 @@ const EditProfilePage = () => {
   return (
     <div className="h-full">
       <GNB />
-      <div
-        className="flex flex-col justify-center items-center gap-10 min-h-[80vh]"
-        style={{
-          backgroundImage: 'url(assets/images/backgroundImage.png)',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        }}
-      >
-        <Banner className="font-semibold text-2xl">My 보호소 정보 수정</Banner>
-
-        <form
-          className="flex flex-col gap-6 w-full max-w-[400px] px-2"
-          onSubmit={handleSubmit}
+      {isLoading ? (
+        <EditProfilePageSkeleton />
+      ) : (
+        <div
+          className="flex flex-col justify-center items-center gap-10 min-h-[80vh]"
+          style={{
+            backgroundImage: 'url(assets/images/backgroundImage.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
         >
-          <InputGroup
-            id="shelter"
-            name="보호소 이름"
-            type="text"
-            placeholder="보호소 이름을 입력해주세요."
-            onChange={() => {}}
-            autocomplete="off"
-            defaultValue={data?.response.shelter.name}
-          />
-          <InputGroup
-            id="shelter-contact"
-            name="보호소 연락처"
-            type="text"
-            placeholder="보호소에 연락 가능한 연락처를 입력해주세요."
-            onChange={() => {}}
-            autocomplete="off"
-            defaultValue={data?.response.shelter.contact}
-          />
-          {/*  defaultValue=data.shelter.address */}{' '}
-          {/* 내부 구조 바꾸는 작업 필요 => props 받는 방식을 바꾸면 될 듯 */}
-          <EditAddressInputGroup />
-          <button className="bg-brand-color text-white w-full rounded-md p-2">
-            {isButtonLoading ? (
-              <ClipLoader size={20} color="#fff" loading={isButtonLoading} />
-            ) : (
-              '정보 수정하기'
-            )}
-          </button>
-        </form>
-      </div>
+          <Banner className="font-semibold text-2xl mb-4">
+            My 보호소 정보 수정
+          </Banner>
+
+          <form
+            className="flex flex-col gap-6 w-full max-w-[400px] px-2"
+            onSubmit={handleSubmit}
+          >
+            <InputGroup
+              id="shelter"
+              name="보호소 이름"
+              type="text"
+              placeholder="보호소 이름을 입력해주세요."
+              onChange={() => {}}
+              autocomplete="off"
+              defaultValue={data?.response.shelter.name}
+            />
+            <InputGroup
+              id="shelter-contact"
+              name="보호소 연락처"
+              type="text"
+              placeholder="보호소에 연락 가능한 연락처를 입력해주세요."
+              onChange={() => {}}
+              autocomplete="off"
+              defaultValue={data?.response.shelter.contact}
+            />
+            <EditAddressInputGroup />
+            <button className="bg-brand-color text-white w-full rounded-md p-2">
+              {isButtonLoading ? (
+                <ClipLoader size={20} color="#fff" loading={isButtonLoading} />
+              ) : (
+                '정보 수정하기'
+              )}
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
