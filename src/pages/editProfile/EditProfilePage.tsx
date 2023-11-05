a3777081e2583b7cdfd7fb4bdc2659a621795d33
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-05 15:58:18 +0900

    feat: 백엔드 api 적용
    백엔드 api로 shelterInfo에 값을 받아서 defaultvalue를 보여주도록 했습니다.
    정보 수정하기 버튼을 누르면 shelterInfo가 변경되도록 구현했습니다.

--- src/pages/editProfile/EditProfilePage.tsx
+++ src/pages/editProfile/EditProfilePage.tsx
@@ -2,83 +2,108 @@ import { useQuery } from '@tanstack/react-query';
 import Banner from 'commons/Banner';
 import InputGroup from 'commons/InputGroup';
 import GNB from 'layouts/GNB';
-import AddressInputGroup from 'pages/signUp/AddressInputGroup';
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ClipLoader } from 'react-spinners';
+import { useRecoilState } from 'recoil';
+import { shelterSignupState } from 'recoil/shelterState';
+import { useEffect } from 'react';
+import { getCookie } from 'commons/cookie/cookie';
+import EditAddressInputGroup from './EditAddressInputGroup';
 
 const EditProfilePage = () => {
   const params = useParams();
   const shelterId = params.id;
-  const [editEmailCheck, setEditEmailCheck] = useState<boolean>(false);
-  const [editPasswordCheck, setEditPasswordCheck] = useState<boolean>(false);
+  const [shelterInfo, setShelterInfo] = useRecoilState(shelterSignupState);
 
-  // 백엔드에 api 문의 필요
   const getProfileInfo = async () => {
     const response = await fetch(
-      `${process.env.REACT_APP_URI}/shelter/${shelterId}`,
+      `${process.env.REACT_APP_URI}/shelter/${shelterId}?page=1`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
     );
+
+    if (!response.ok) {
+      if (response.status === 500) {
+        console.error('서버측 에러');
+      } else {
+        console.error('내부 에러 : 알 수 없음');
+      }
+    }
+
+    return response.json();
   };
+
+  const shelterFetch = async () => {
+    const token = getCookie('loginToken');
+    const response = await fetch(
+      `${process.env.REACT_APP_URI}/shelter/${shelterId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+          name: shelterInfo.name,
+          contact: shelterInfo.contact,
+          shelterAddressUpdateDto: shelterInfo.address,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      if (response.status === 500) {
+        console.error('서버측 에러');
+      } else {
+        console.error('내부 에러 : 알 수 없음');
+      }
+    }
+
+    return response.json();
+  };
+
+  const handleSubmit = () => {
+    shelterFetch();
+  };
+
   const { data } = useQuery({
     queryKey: ['editProfile', shelterId],
-    queryFn: () => {},
+    queryFn: getProfileInfo,
   });
 
+  useEffect(() => {
+    setShelterInfo({
+      ...shelterInfo,
+      name: data?.response.shelter.name,
+      contact: data?.response.shelter.contact,
+      address: {
+        ...data?.response.shelter.address,
+      },
+    });
+  }, [data]);
+
   return (
-    <div>
+    <div className="h-full">
       <GNB />
       <div
-        className="flex flex-col justify-center items-center gap-4 min-h-screen"
+        className="flex flex-col justify-center items-center gap-10 min-h-[80vh]"
         style={{
           backgroundImage: 'url(assets/images/backgroundImage.png)',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
         }}
       >
-        <div className="flex flex-col gap-2">
-          <Banner className="font-semibold text-2xl">
-            My 보호소 정보 수정
-          </Banner>
-        </div>
+        <Banner className="font-semibold text-2xl">My 보호소 정보 수정</Banner>
+
         <form
-          className="flex flex-col gap-3 w-full max-w-[400px] px-2"
-          onSubmit={() => {}}
+          className="flex flex-col gap-6 w-full max-w-[400px] px-2"
+          onSubmit={handleSubmit}
         >
-          <div className="email-confirm flex place-items-end justify-center">
-            <InputGroup
-              id="email"
-              name="이메일"
-              type="text"
-              placeholder="이메일을 입력해주세요."
-              onChange={() => {}}
-              autocomplete="off"
-            />
-            <button
-              type="button" // type을 버튼으로 지정해주면 handleSubmit이 작동하지 않음 -> onClick만 동작
-              className="bg-brand-color text-white rounded min-w-[100px] min-h-[44px]"
-              onClick={() => {}}
-            >
-              중복 확인
-            </button>
-          </div>
-          <InputGroup
-            id="password"
-            name="비밀번호"
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            onChange={() => {}}
-            autocomplete="off"
-          />
-
-          <InputGroup
-            id="password-confirm"
-            name="비밀번호 확인"
-            type="password"
-            placeholder="비밀번호를 한번 더 입력해주세요."
-            onChange={() => {}}
-            autocomplete="off"
-          />
-
           <InputGroup
             id="shelter"
             name="보호소 이름"
@@ -86,8 +111,8 @@ const EditProfilePage = () => {
             placeholder="보호소 이름을 입력해주세요."
             onChange={() => {}}
             autocomplete="off"
+            defaultValue={data?.response.shelter.name}
           />
-
           <InputGroup
             id="shelter-contact"
             name="보호소 연락처"
@@ -95,16 +120,19 @@ const EditProfilePage = () => {
             placeholder="보호소에 연락 가능한 연락처를 입력해주세요."
             onChange={() => {}}
             autocomplete="off"
+            defaultValue={data?.response.shelter.contact}
           />
-
-          <AddressInputGroup />
+          {/*  defaultValue=data.shelter.address */}{' '}
+          {/* 내부 구조 바꾸는 작업 필요 => props 받는 방식을 바꾸면 될 듯 */}
+          <EditAddressInputGroup />
           <button className="bg-brand-color text-white w-full rounded-md p-2">
-            {'isLoading' ? (
+            {/* {isLoading ? (
               // false -> isLoading으로 넣기
               <ClipLoader size={20} color="#fff" loading={false} />
             ) : (
-              '회원가입'
-            )}
+              '정보 수정하기'
+            )} */}
+            정보 수정하기
           </button>
         </form>
       </div>
