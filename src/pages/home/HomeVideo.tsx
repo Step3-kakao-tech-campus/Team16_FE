b9a6900dc2b8f0ff5e325d86c2285b143f5895d3
hjiwon <ba0061@naver.com>
2023-11-09 23:32:56 +0900

    refactor: 비디오의 위에 뜨는 모든 요소들 하나의 컴포넌트로 묶어줌

--- src/pages/home/HomeVideo.tsx
+++ src/pages/home/HomeVideo.tsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
 import VideoDragBar from './VideoDragBar';
-import InstructBox from './InstructBox';
 
 export interface HomeVideoProps {
   url: string;
@@ -82,29 +81,34 @@ const HomeVideo = (props: HomeVideoProps) => {
       videoPlayerRef.current.pause();
     }
   };
+  const handleMouseEnter = () => {
+    setHovering(true);
+    setOpacity(1);
+    setTimeout(() => {
+      setHovering(false);
+    }, 1500);
+    setTimeout(() => {
+      setOpacity(0);
+    }, 1300);
+  };
   return (
-    <div onClick={handleVideoClick}>
+    <>
       {hovering && <VideoDragBar opacity={opacity} />}
       {((loading && index === 0) || (loading && index !== 0 && !playing)) && (
-        <InstructBox />
+        <div className="absolute w-fit rounded-2xl h-fit p-10 backdrop-blur-lg flex flex-col justify-center items-center align-middle bg-brand-color/50">
+          <div className="text-white">한 번 탭하면 일시정지</div>
+          <div className="text-white">두 번 탭하면 음소거가 가능해요</div>
+        </div>
       )}
       {loading && index !== 0 && playing && (
         <div className="absolute w-10 h-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 loader" />
       )}
       <div
         ref={videoRef}
+        onClick={handleVideoClick}
         onDoubleClick={handleDoubleClick}
         className="h-[70vh] items-center justify-center"
-        onMouseEnter={() => {
-          setHovering(true);
-          setOpacity(1);
-          setTimeout(() => {
-            setHovering(false);
-          }, 1500);
-          setTimeout(() => {
-            setOpacity(0);
-          }, 1300);
-        }}
+        onMouseEnter={handleMouseEnter}
       >
         <video
           className="w-full h-full"
@@ -117,7 +121,7 @@ const HomeVideo = (props: HomeVideoProps) => {
           <source src={url} type="video/mp4" />
         </video>
       </div>
-    </div>
+    </>
   );
 };
 
