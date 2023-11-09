4b6034b0d35fecdd823676c675095143be8d7239
hjiwon <ba0061@naver.com>
2023-11-09 23:27:05 +0900

    refactor: 컴포넌트 분리

--- src/pages/home/HomeVideo.tsx
+++ src/pages/home/HomeVideo.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import VideoDragBar from './VideoDragBar';
+import InstructBox from './InstructBox';
 
 export interface HomeVideoProps {
   url: string;
@@ -85,10 +86,7 @@ const HomeVideo = (props: HomeVideoProps) => {
     <div onClick={handleVideoClick}>
       {hovering && <VideoDragBar opacity={opacity} />}
       {((loading && index === 0) || (loading && index !== 0 && !playing)) && (
-        <div className="absolute w-fit rounded-2xl h-fit p-10 backdrop-blur-lg flex flex-col justify-center items-center align-middle bg-brand-color/50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="text-white">한 번 탭하면 재생 / 일시정지</div>
-          <div className="text-white">두 번 탭하면 음소거가 가능해요</div>
-        </div>
+        <InstructBox />
       )}
       {loading && index !== 0 && playing && (
         <div className="absolute w-10 h-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 loader" />
