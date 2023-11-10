6c19cb6d61fe5c6cd0bf7298294314a398acabca
hjiwon <ba0061@naver.com>
2023-11-10 19:13:27 +0900

    feat: 요소 위치조정

--- src/pages/home/components/VideoOverlay.tsx
+++ src/pages/home/components/VideoOverlay.tsx
@@ -7,7 +7,7 @@ const VideoOverlay = (props: VideoOverlayProps) => {
     <>
       {hovering && <VideoDragBar opacity={opacity} />}
       {((loading && index === 0) || (loading && index !== 0 && !playing)) && (
-        <div className="absolute w-fit rounded-2xl h-fit p-10 backdrop-blur-lg flex flex-col justify-center items-center align-middle bg-brand-color/50">
+        <div className="absolute w-fit rounded-2xl h-fit p-10 backdrop-blur-lg flex flex-col justify-center items-center align-middle bg-brand-color/50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className="text-white">한 번 탭하면 일시정지</div>
           <div className="text-white">두 번 탭하면 음소거가 가능해요</div>
         </div>
