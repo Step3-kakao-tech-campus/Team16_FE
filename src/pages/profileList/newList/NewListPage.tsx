6aab386ce5fa3b299eedc16063c06b79dc68131d
LimSumi <thddkwl5330@naver.com>
2023-11-10 21:50:29 +0900

    fix: 프로필리스트 ui 변경

--- src/pages/profileList/newList/NewListPage.tsx
+++ src/pages/profileList/newList/NewListPage.tsx
@@ -1,11 +1,14 @@
 import NewList from 'pages/profileList/newList/NewList';
 import GNB from 'layouts/GNB';
+import ErrorBoundary from 'commons/ErrorBoundary';
 
 const NewListPage = () => {
   return (
     <div>
       <GNB />
-      <NewList />
+      <ErrorBoundary>
+        <NewList />
+      </ErrorBoundary>
     </div>
   );
 };
--- src/pages/profileList/urgentList/UrgentListSkeleton.tsx
+++ src/pages/profileList/urgentList/UrgentListSkeleton.tsx
@@ -4,15 +4,15 @@ const UrgentListSkeleton = () => {
       <h2 className="flex w-full font-bold text-xl sm:text-2xl items-center whitespace-nowrap">
         긴급 도움이 필요해요!
       </h2>
-      <div className="grid grid-cols-1 gap-1 md:grid-cols-2 my-10 w-full whitespace-nowrap">
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
-        <div className="w-52 h-24 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+      <div className="grid grid-cols-1 gap-1 md:grid-cols-2 my-3 w-full whitespace-nowrap">
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
+        <div className="w-64 h-28 bg-gray-200 m-2 flex items-center gap-6 ml-20" />
       </div>
       <div className="flex justify-center mb-11 sm:mb-28"></div>
     </div>
