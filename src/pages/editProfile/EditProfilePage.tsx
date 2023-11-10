598d8997598f386f89d1db4a47fbb10726802cea
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-11 06:52:15 +0900

    feat: 보호소 정보 수정 페이지 에러 캐칭 적용
    get, put 방식 모두 적용

--- src/pages/editProfile/EditProfilePage.tsx
+++ src/pages/editProfile/EditProfilePage.tsx
@@ -1,70 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { shelterSignupState } from 'recoil/shelterState';
-import { getCookie } from 'commons/cookie/cookie';
-import usePostFetch from 'commons/apis/usePostFetch';
-import useGetFetch from 'commons/apis/useGetFetch';
-import { useEffect } from 'react';
-import VEditProfilePage from './VEditProfilePage';
+import React from 'react';
+import ErrorBoundary from 'layouts/ErrorBoundary';
+import EditProfileTemplate from './components/EditProfileTemplate';
 
 const EditProfilePage = () => {
-  const params = useParams();
-  const shelterId = params.id;
-  const token = getCookie('loginToken');
-  const [shelterInfo, setShelterInfo] = useRecoilState(shelterSignupState);
-
-  const { getStatusCode, getLoading, getData } = useGetFetch(
-    `${process.env.REACT_APP_URI}/shelter/${shelterId}?page=1`,
+  return (
+    <ErrorBoundary fallback={'Error...'}>
+      <EditProfileTemplate />
+    </ErrorBoundary>
   );
-
-  const { postStatusCode, postloading, postData } = usePostFetch(
-    `${process.env.REACT_APP_URI}/shelter/${shelterId}`,
-    {
-      method: 'PUT',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-
-      body: JSON.stringify({
-        name: shelterInfo.name,
-        contact: shelterInfo.contact,
-        shelterAddressUpdateDto: shelterInfo.address,
-      }),
-    },
-  );
-
-  const mutation = useMutation(getData, {
-    onSuccess: (info) => {
-      setShelterInfo({
-        ...shelterInfo,
-        name: info.response.shelter.name,
-        contact: info.response.shelter.contact,
-        address: {
-          ...info.response.shelter.address,
-        },
-      });
-    },
-  });
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    postData();
-  };
-
-  useEffect(() => {
-    mutation.mutate();
-  }, []);
-
-  const EditProfileProps = {
-    getLoading,
-    postloading,
-    handleSubmit,
-    shelterInfo,
-  };
-
-  return <VEditProfilePage {...EditProfileProps} />;
 };
 
 export default EditProfilePage;
--- src/pages/editProfile/editProfileType.d.ts
+++ src/pages/editProfile/editProfileType.d.ts
@@ -1,5 +1,9 @@
 export interface VEditProfileProps {
   getLoading: boolean;
   postloading: boolean;
+  modalOpen: boolean;
+  modalText: string;
+  handleModalClose: () => void;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
