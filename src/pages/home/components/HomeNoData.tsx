ab86ce2b33ec32a255b726cd922f0e0f3678a964
hjiwon <ba0061@naver.com>
2023-11-10 18:07:38 +0900

    feat: 동영상 없을 때 문구 추가

--- src/pages/home/components/HomeNoData.tsx
+++ src/pages/home/components/HomeNoData.tsx
@@ -2,17 +2,40 @@ const HomeNoData = ({ species }: { species: string }) => {
   return (
     <>
       {species === '강아지' && (
-        <img src="/assets/images/dog.png" alt="강아지" className="grayscale" />
+        <img
+          src="/assets/images/dog.png"
+          alt="강아지"
+          className="grayscale absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{ width: '14rem', height: '14rem' }}
+        />
       )}
       {species === '고양이' && (
-        <img src="/assets/images/cat.png" alt="고양이" className="grayscale" />
+        <img
+          src="/assets/images/cat.png"
+          alt="고양이"
+          className="grayscale absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{ width: '14rem', height: '14rem' }}
+        />
       )}
       {species === '기타' && (
-        <img src="/assets/images/racoon.png" alt="기타" className="grayscale" />
+        <img
+          src="/assets/images/racoon.png"
+          alt="기타"
+          className="grayscale absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{ width: '14rem', height: '14rem' }}
+        />
       )}
       {species === '전체' && (
-        <img src="/assets/images/animal.png" alt="전체" className="grayscale" />
+        <img
+          src="/assets/images/all.png"
+          alt="전체"
+          className="grayscale absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{ width: '14rem', height: '14rem' }}
+        />
       )}
+      <h1 className="text-xl font-bold text-center absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        영상이 없습니다... 😢
+      </h1>
     </>
   );
 };
