2e7da65aa52df5c1b8733d71b4a4211b1cc0a421
LimSumi <thddkwl5330@naver.com>
2023-11-11 15:11:10 +0900

    feat: 보호소 프로필 보호중 동물 없을 경우 추가

--- src/pages/shelterInfo/components/VShelterInfo.tsx
+++ src/pages/shelterInfo/components/VShelterInfo.tsx
@@ -3,12 +3,12 @@ import ProfileCard from 'pages/profileList/components/ProfileCard';
 import { getCookie } from 'commons/cookie/cookie';
 import VShelterCard from './VShelterCard';
 import { ShelterInfoPageProps } from '../shelterInfoType';
+import ShelterNoData from './ShelterNoData';
 
 const VShelterInfo = (props: ShelterInfoPageProps) => {
   const loginAccount = getCookie('accountInfo');
   const role = loginAccount ? loginAccount.split(' ')[0] : '';
   const id = loginAccount ? loginAccount.split(' ')[1] : '';
-
   return (
     <div>
       <div className="mt-8 sm:mt-20 flex justify-center ">
@@ -18,37 +18,50 @@ const VShelterInfo = (props: ShelterInfoPageProps) => {
         <h2 className="flex w-full font-bold text-xl sm:text-2xl items-center mb-5 whitespace-nowrap">
           관리중인 동물
         </h2>
-        <div className="grid grid-cols-1 items-center place-items-center gap-1 md:grid-cols-2 my-1 w-full">
-          {props.profileProps.map((item, index) => (
-            <div className="flex flex-row" key={index}>
-              <ProfileCard
-                key={index}
-                petId={item.id}
-                petName={item.name}
-                petAge={item.age}
-                shelterName={''}
-                {...item}
-              />
-              <button
-                className={`${
-                  loginAccount &&
-                  role === 'SHELTER' &&
-                  id === `${props.shelterInfoProps.id}`
-                    ? ' absolute ml-60 bg-slate-200 text-sm h-fit w-fit p-1  rounded-xl '
-                    : ' text-transparent opacity-0'
-                }`}
-                onClick={() => {
-                  // eslint-disable-next-line no-restricted-globals
-                  location.href = `/pet-update/${item.id}`;
-                }}
-              >
-                수정하기
-              </button>
-            </div>
-          ))}
-        </div>
+        {props.pageNationProps.lastPage === 0 ? (
+          <div>
+            <ShelterNoData />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 items-center place-items-center gap-1 md:grid-cols-2 my-1 w-full">
+            {props.profileProps.map((item, index) => (
+              <div className="flex flex-row" key={index}>
+                <ProfileCard
+                  key={index}
+                  petId={item.id}
+                  petName={item.name}
+                  petAge={item.age}
+                  shelterName={''}
+                  status={item.adoptionStatus === 'YES' ? '입양완료' : '미입양'}
+                  {...item}
+                />
+                <button
+                  className={`${
+                    loginAccount &&
+                    role === 'SHELTER' &&
+                    id === `${props.shelterInfoProps.id}`
+                      ? ' absolute ml-60 bg-slate-200 text-sm h-fit w-fit p-1  rounded-xl '
+                      : ' text-transparent opacity-0'
+                  }`}
+                  onClick={() => {
+                    // eslint-disable-next-line no-restricted-globals
+                    location.href = `/pet-update/${item.id}`;
+                  }}
+                >
+                  수정하기
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      <div className="flex justify-center mt-5 mb-11 sm:mb-28">
+      <div
+        className={`${
+          props.pageNationProps.lastPage !== 0
+            ? ' flex justify-center mt-5 mb-11 sm:mb-28 '
+            : ' text-transparent opacity-0'
+        }`}
+      >
         <Pagination
           currentPage={props.pageNationProps.currentPage}
           lastPage={props.pageNationProps.lastPage}
