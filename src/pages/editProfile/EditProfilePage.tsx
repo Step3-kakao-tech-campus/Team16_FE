16d1cac733acb698adf01130c22ba1e559ccf20d
JeonDoGyun <wjsehrbs2632@naver.com>
2023-11-11 14:28:32 +0900

    feat: Loading 화면이 필요한 lazy loading 방식의 페이지에 Suspense 추가

--- src/pages/editProfile/EditProfilePage.tsx
+++ src/pages/editProfile/EditProfilePage.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Suspense } from 'react';
 import ErrorBoundary from 'layouts/ErrorBoundary';
+import { ClipLoader } from 'react-spinners';
 import EditProfileTemplate from './components/EditProfileTemplate';
 
 const EditProfilePage = () => {
   return (
     <ErrorBoundary fallback={'Error...'}>
-      <EditProfileTemplate />
+      <Suspense
+        fallback={<ClipLoader className="absolute top-[50%] left-{50%}" />}
+      >
+        <EditProfileTemplate />
+      </Suspense>
     </ErrorBoundary>
   );
 };
--- src/pages/profileList/profileListHome/ProfileListPage.tsx
+++ src/pages/profileList/profileListHome/ProfileListPage.tsx
@@ -1,10 +1,16 @@
 import ErrorBoundary from 'layouts/ErrorBoundary';
 import ProfileListHome from './ProfileListHome';
+import { Suspense } from 'react';
+import { ClipLoader } from 'react-spinners';
 
 const ProfileListPage = () => {
   return (
     <ErrorBoundary>
-      <ProfileListHome />
+      <Suspense
+        fallback={<ClipLoader className="absolute top-[50%] left-{50%}" />}
+      >
+        <ProfileListHome />
+      </Suspense>
     </ErrorBoundary>
   );
 };
